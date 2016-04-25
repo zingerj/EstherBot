6280dd67e6d5ead50db67ee87e02443e46832947
Jonathan Zinger <jzinger1@babson.edu>
2016-04-25 16:53:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Jonathan? Just say hello to get started!')
+            return bot.say('So you want to learn about Jonathan? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
